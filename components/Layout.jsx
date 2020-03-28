e084c4817721941e961116709ef8dea69c090908
luopeihai2019 <luopeihai2019@163.com>
2020-03-28 23:58:25 +0800

    layout 页 ui 完成

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,3 +1,4 @@
+import { useState, useCallback } from "react";
 import Link from "next/link";
 import {
   Layout,
@@ -11,18 +12,97 @@ import {
 } from "antd";
 const { Header, Content, Footer } = Layout;
 
-export default ({ children }) => (
-  <Layout>
-    <Header></Header>
-    <div className="header-left">
-      <div className="logo">
-        <Icon type="github" />
-      </div>
-      <div>
-        <Input.Search placeholder="搜索仓库" />
-      </div>
-    </div>
-    <Content>{children}</Content>
-    <Footer>Develop by Jokcy teacher</Footer>
-  </Layout>
-);
+const githubIconStyle = {
+  color: "white",
+  fontSize: 40,
+  display: "block",
+  paddingTop: 10,
+  marginRight: 20
+};
+
+const footerStyle = {
+  textAlign: "center"
+};
+
+export default ({ children }) => {
+  const [search, setSearch] = useState("");
+  const handleSearchChange = useCallback(event => {
+    setSearch(event.target.value);
+  }, []);
+  const handleOnSearch = useCallback(() => {}, []);
+
+  return (
+    <Layout>
+      <Header>
+        <div className="header-inner">
+          <div className="header-left">
+            <div className="logo">
+              <Icon type="github" style={githubIconStyle} />
+            </div>
+            <div>
+              <Input.Search
+                placeholder="搜索仓库"
+                value={search}
+                onChange={handleSearchChange}
+                onSearch={handleOnSearch}
+              />
+            </div>
+          </div>
+          <div className="header-right">
+            <div className="user">
+              <Avatar size={40} icon="user"></Avatar>
+            </div>
+          </div>
+        </div>
+      </Header>
+
+      <Content>{children}</Content>
+      <Footer style={footerStyle}>Develop by Jokcy teacher</Footer>
+      <style jsx global>
+        {`
+          #__next {
+            height: 100%;
+          }
+
+          .ant-layout {
+            min-height: 100%;
+          }
+
+          .ant-layout-header {
+            padding-left: 0;
+            padding-right: 0;
+          }
+
+          .ant-layout-content {
+            background: #fff;
+          }
+        `}
+      </style>
+      <style jsx>
+        {`
+          .header-inner {
+            display: flex;
+            justify-content: space-between;
+          }
+
+          .header-left {
+            display: flex;
+            justify-content: flex-start;
+          }
+
+          :global(.icon-github) {
+            display: block;
+            padding-top: 10px;
+            margin-right: 20px;
+            color: #fff;
+            font-size: 40px;
+          }
+
+          :global(.footer) {
+            text-align: center;
+          }
+        `}
+      </style>
+    </Layout>
+  );
+};
