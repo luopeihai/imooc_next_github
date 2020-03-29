3798c8cf9adc07bcb60320fd56b63cefb85c6f82
luopeihai2019 <luopeihai2019@163.com>
2020-03-29 22:33:12 +0800

    请求接口 区分

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-export default () => <span>Index</span>;
+import axios from "axios";
+const api = require("../lib/api");
+
+function Index() {
+  return <span>Index</span>;
+}
+
+Index.getInitialProps = async ({ ctx }) => {
+  const result = await api.request();
+};
+
+export default withRouter(Index);
